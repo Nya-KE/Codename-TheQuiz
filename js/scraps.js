a387007726d231e174d4889df7c3203ad0ad877e
Nyakio Mburu <nyakio.mburu@gmail.com>
2018-09-25 20:30:55 +0300

    adds one question and it's accompanying answers

--- js/scraps.js
+++ js/scraps.js
@@ -1,79 +1,62 @@
 //Business Logic
-function Place(greeting,country,location,landmarks, timeYear, notes, images, food, weather ) {
-    this.greeting = greeting,
-    this.country = country,
-    this.location = location,
-    this.landmarks = landmarks,
-    this.timeYear = timeYear,
-    this.notes = notes,
-    this.images = images,
-    this.food = food
-    this.weather = weather
+function Options(question,a,b,c,d) {
+    this.question = question,
+    this.opt1 = a,
+    this.opt2 = b,
+    this.opt3 = c,
+    this.opt4 = d,
 }
 
-var greece  = new Place("καλωσόρισμα","Greece","Athens",["Acropolis","Parthenon"],"Summer-Time","The women were goddesses!","images/greece.JPEG","Moussaka", "Humid")
+var ques1 = new Options("What is the other name for JavaScript?","ECMAScript","Vue","Java","None of the above")
 
-var japan = new Place("Yokoso","Japan","Kyoto",["Fushimi Inari Taisha","Arashiyama"],"Spring-Time","The smell of blossoms drifting in the wind is unforgettable!","images/japan.JPEG","Sushi", "Rainy")
+var ques2 = new Options("What is most true about a constructor?","It is an element of JavaScript which allows prototypal inheritance", "A constructor is a method used to create objects", "A constructor is used to create objects", "A constructor creates and initializes an object within an id"
 
-var hawaii = new Place("Welina","Hawaii","Maui",["Molokini","Iao Valley"],"End of Year","Learned how to dance the hula!","images/hawaii.JPEG","Kalua Pork","hot")
+var ques3 = new Options("I have multiple elements which I would like to encapsulate. What do I use?","An array", "An object", "A multi-string", "None of the above")
 
-Place.prototype.clicks = function(){
+var ques4  = new Options("What way can JS code be debugged?","Use console.log to check for breaks", "Confirming file paths for fetched documents i.e .js, .css", "Unminify your code", "All of the above")
+
+Options.prototype.clicks = function(){
     var keys = Object.keys(this)
     var values = Object.values(this)
     for (var i = 0; i < keys.length; i++) {
-    /*  if (keys[i]="images") {
-        $(".about").append("<li class='info'>"+ keys[i] + ": "+ "<img src=' "+ values[i] +"' alt='" + keys[0] + "'>" + "</li>")
-      } else if (keys[i] = "greeting"){(INSERT)} */
-        $(".about").append("<li class='info'>" + keys[i] + ": "+ values[i]+ "</li>")
-       //end of else
+        if (i = 0) { $("p.answer").append(values[0])
+      } else {
+        $("input::checked").val();
+        $(".answer").append("<li>" + keys[i] + ". "+ "<label><input type='radio  value="ECMAScript'checked>"
+          values[i]
+        "</label>" + "</li>")
+      }
     };
 }
 
 // User Interface Logic
 $(document).ready(function(){
-  $(".greece").click(function(){
-    $("li.info").remove();
-    greece.clicks();
-  }) //end of greece button
-  $(".japan").click(function(){
-    $("li.info").remove();
-    japan.clicks();
-  }) //end of japan button
-  $(".hawaii").click(function(){
-    $("li.info").remove();
-    hawaii.clicks();
-  }) //end of hawaii button
+
+  $(".button").click(function(){
+    //$("li.opt").remove();
+    ques1.clicks();
+  }) //end of ques1 button
+/*
+  $(".button").click(function(){
+    $("li.opt").remove();
+    ques2.clicks();
+  }) //end of ques2 button
+  $(".button").click(function(){
+    $("li.opt").remove();
+    ques3.clicks();
+  }) //end of ques3 button
+*/
 })
 
-
-
-
 /*
-Place.prototype.forEach = function(){
-    for (var index = 0; index < greece.length; index ++ ) {
+      <div class="questions">
+          <label>
+            <input type="" value="ECMAScript" checked>
+            ECMAScript
+          </label>
+      </div>
 
-    }
-}
-*/
+      var flavor = $("input:q1[value=ECMAScript]:checked").val();
+                Cookies & Cream
 
-/*
-for (var property in greece) {
-  // if greece.hasOwnProperty(property)){
-  $(".about").append("<li>" + greece.keys + "</li>")
-  // }
-} */
- accompanying html
-
- <div class="col-md-12">
-   <h3>Question</h3>
-   <p class="answer"></p>
-   <ul class="answer">
-     <li>
-       <div class="questions">
-           <label>
-             <input type="radio" checked>
-             ECMAScript
-           </label>
-       </div>
-     </li>
-   </ul>
+      */
