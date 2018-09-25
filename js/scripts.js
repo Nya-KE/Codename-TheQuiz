5cf040a2926e2c96eb554173f27e637e710916c6
Nyakio Mburu <nyakio.mburu@gmail.com>
2018-09-25 16:09:44 +0300

    adds initial business logic idea for using constructors method

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//Business Logic
+function Options(a,b,c,d) {
+    this.opt= a,
+    this.opt2 = b,
+    this.opt3 = c,
+    this.opt4 = d,
+}
+
+var ques1 = new Options("ECMAScript","Vue","Java","None of the above")
+
+var ques2 = new Options("It is an element of JavaScript which allows prototypal inheritance", "A constructor is a method used to create objects", "A constructor is used to create objects", "A constructor creates and initializes an object within an id"
+
+var ques3 = new Options("An array", "An object", "A multi-string", "None of the above")
+
+var ques4  = new Options("Use console.log to check for breaks", "Confirming file paths for fetched documents i.e .js, .css", "Unminify your code", "All of the above")
