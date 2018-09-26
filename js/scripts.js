9d8cc77bbaae65b27a4fb94d5348d46a651ce580
Nyakio Mburu <nyakio.mburu@gmail.com>
2018-09-26 15:08:47 +0300

    adds functional scoring and edits README with known bug criterio as well as shout-out to instrumental people in this project

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,57 @@
 //Business Logic
 
+function mark(){
+  var ans1 = $("input:radio[name=ques1]:checked").val();
+  var ans2 = $("input:radio[name=ques2]:checked").val();
+  var ans3 = $("input:radio[name=ques3]:checked").val();
+  var ans4 = $("input:radio[name=ques4]:checked").val();
+  var ans5 = $("input:radio[name=ques5]:checked").val();
+  var answerSum = 0;
+  if (ans1 == "correct"){
+    answerSum += 1;
+  };
+  if (ans2 == "correct"){
+    answerSum += 3;
+  };
+  if (ans3 == "correct"){
+    answerSum += 2;
+  };
+  if (ans4 == "correct"){
+    answerSum += 2.5;
+  };
+  if (ans5 == "correct"){
+    answerSum += 2;
+  };
+  alert(answerSum)
+  $("p#result").append(answerSum);
+
+};
+
+
+
 //fetch values
 /* paste form "<form name="questions">  </form>" in HTML as this enables the below filepath using names to work
 function check(){
-  var ques1 = document.questions.ques1.value;
-  var ques2 = document.questions.ques2.value;
-  var ques3 = document.questions.ques3.value;
-  var ques4 = document.questions.ques4.value;
-  var ques5 = document.questions.ques5.value;
+  var ans1 = document.questions.ques1.value;
+  var ans2 = document.questions.ques2.value;
+  var ans3 = document.questions.ques3.value;
+  var ans4 = document.questions.ques4.value;
+  var ans5 = document.questions.ques5.value;
   var answersum = 0
 }
-if (ques1 == "correct"){
+if (ans1 == "correct"){
   answersum += 2;
 };
-if (ques2 == "correct"){
+if (ans2 == "correct"){
   answersum += 2;
 };
-if (ques3 == "correct"){
+if (ans3 == "correct"){
   answersum += 2;
 };
-if (ques4 == "correct"){
+if (ans4 == "correct"){
   answersum += 2;
 };
-if (ques5 == "correct"){
+if (ans5 == "correct"){
   answersum += 2;
 };
 */
@@ -59,6 +88,7 @@ $(document).ready(function(){
     $("div#scoresheet").hide()
     $("div#score").show()
     });
+
 })
 /*<div id="ques">
   <center><p>" + keys[i] + "</p>
